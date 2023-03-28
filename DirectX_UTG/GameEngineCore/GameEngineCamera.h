807eaae56c7474b696dda31a8e73c0a21050f5a5
엄태건 <umtimos@naver.com>
2023-03-28 21:17:07 +0900

    .

--- DirectX_UTG/GameEngineCore/GameEngineCamera.h
+++ DirectX_UTG/GameEngineCore/GameEngineCamera.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "GameEngineActor.h"
 
-// 설명 :
+// 설명 : 뷰 행렬을 활용하기 위한 카메라 액터, 레벨 생성 시 maincamera가 shared_ptr로 즉시 하나 생성된다(GELevel의 생성자 단계에서 생성됨).
 class GameEngineCamera : public GameEngineActor
 {
 public:
